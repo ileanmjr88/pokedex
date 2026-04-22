ec4bb36d21d83afbc644d98b40c7bbe8f1bfac83
Ilean Monterrubio Jr <ilean.monterrubio@icloud.com>
2026-04-22 07:55:30 -0500

    Initial Vite + React + TypeScript scaffold

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { PokemonGrid } from './components/PokemonGrid'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Pokédex</h1>
+      <PokemonGrid />
+    </div>
+  )
+}
+
+export default App
